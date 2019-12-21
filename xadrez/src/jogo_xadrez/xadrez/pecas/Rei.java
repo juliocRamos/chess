4a41a1d5aba164952cf7154d227f85bc63566d3e
Júlio Ramos <ps2.ramos@gmail.com>
2019-12-21 16:31:57 -0300

    Implementados os movimentos possíveis do Rei
    Motivo: I/M (M - Melhoria | C - Correção | I - Implementação)
    
    Descrição:
      - Implementado o processamento específico para mover a peça Rei.
    
    Observações:
    
    Referência:

--- xadrez/src/jogo_xadrez/xadrez/pecas/Rei.java
+++ xadrez/src/jogo_xadrez/xadrez/pecas/Rei.java
@@ -1,5 +1,6 @@
 package jogo_xadrez.xadrez.pecas;
 
+import jogo_xadrez.boardgame.Posicao;
 import jogo_xadrez.boardgame.Tabuleiro;
 import jogo_xadrez.xadrez.CorPeca;
 import jogo_xadrez.xadrez.PecaXadrez;
@@ -19,6 +20,62 @@ public class Rei extends PecaXadrez {
     public boolean[][] movimentosPossiveis() {
         boolean[][] matTmp
                 = new boolean[getTabuleiro().getLinhas()][getTabuleiro().getColunas()];
+
+        Posicao auxP = new Posicao(0, 0);
+
+        // Acima
+        auxP.setValores(posicao.getLinha() - 1, posicao.getColuna());
+        if (getTabuleiro().posicaoExiste(auxP) && podeSerMovido(auxP)) {
+            matTmp[auxP.getLinha()][auxP.getColuna()] = true;
+        }
+
+        // Abaixo
+        auxP.setValores(posicao.getLinha() + 1, posicao.getColuna());
+        if (getTabuleiro().posicaoExiste(auxP) && podeSerMovido(auxP)) {
+            matTmp[auxP.getLinha()][auxP.getColuna()] = true;
+        }
+
+        // Esquerda
+        auxP.setValores(posicao.getLinha(), posicao.getColuna() - 1);
+        if (getTabuleiro().posicaoExiste(auxP) && podeSerMovido(auxP)) {
+            matTmp[auxP.getLinha()][auxP.getColuna()] = true;
+        }
+
+        // Direita
+        auxP.setValores(posicao.getLinha(), posicao.getColuna() + 1);
+        if (getTabuleiro().posicaoExiste(auxP) && podeSerMovido(auxP)) {
+            matTmp[auxP.getLinha()][auxP.getColuna()] = true;
+        }
+
+        // Diagonal esquerda Superior (Noroeste)
+        auxP.setValores(posicao.getLinha() - 1, posicao.getColuna() - 1);
+        if (getTabuleiro().posicaoExiste(auxP) && podeSerMovido(auxP)) {
+            matTmp[auxP.getLinha()][auxP.getColuna()] = true;
+        }
+
+        // Diagonal esquerda Inferior (Sudoeste)
+        auxP.setValores(posicao.getLinha() + 1, posicao.getColuna() - 1);
+        if (getTabuleiro().posicaoExiste(auxP) && podeSerMovido(auxP)) {
+            matTmp[auxP.getLinha()][auxP.getColuna()] = true;
+        }
+
+        // Diagonal direita Superior (Nordeste)
+        auxP.setValores(posicao.getLinha() - 1, posicao.getColuna() + 1);
+        if (getTabuleiro().posicaoExiste(auxP) && podeSerMovido(auxP)) {
+            matTmp[auxP.getLinha()][auxP.getColuna()] = true;
+        }
+
+        // Diagonal direita Inferior (Sudeste)
+        auxP.setValores(posicao.getLinha() + 1, posicao.getColuna() + 1);
+        if (getTabuleiro().posicaoExiste(auxP) && podeSerMovido(auxP)) {
+            matTmp[auxP.getLinha()][auxP.getColuna()] = true;
+        }
+
         return matTmp;
+    }
+
+    public boolean podeSerMovido(Posicao posicao) {
+        PecaXadrez auxP = (PecaXadrez) getTabuleiro().peca(posicao);
+        return auxP == null || auxP.getCor() != getCor();
     }
 }
